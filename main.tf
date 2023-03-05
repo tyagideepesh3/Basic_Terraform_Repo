fc35051c1e1b4bf61db931a88c42f4381bea5eb4
tyagideepesh3 <64723395+tyagideepesh3@users.noreply.github.com>
2023-03-05 12:01:58 +0530

    module_security_group_commit

--- main.tf
+++ main.tf
@@ -22,11 +22,22 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
-  vpc_security_group_ids = [ aws_security_group.blog.id ]
+  vpc_security_group_ids = [ module.blog_sg.security_group_id ]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source        = "terraform-aws-modules/security-group/aws"
+  version       = "4.17.1"
+  vpc_id        = data.aws_vpc.default.id
+  name          = "blog_new"
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all","https-443-tcp"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
