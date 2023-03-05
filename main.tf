84275dbd4597f81ecafa213fc859d9415a3a7f3f
tyagideepesh3 <64723395+tyagideepesh3@users.noreply.github.com>
2023-03-05 15:04:54 +0530

    changes

--- main.tf
+++ main.tf
@@ -14,17 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-#data "aws_vpc" "default" {
-#    default = true
-#}
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
+
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -35,24 +31,25 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-  vpc_security_group_ids = [ module.blog_sg.security_group_id ]
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
+
 module "blog_sg" {
-  source        = "terraform-aws-modules/security-group/aws"
-  version       = "4.17.1"
-  vpc_id        = module.blog_vpc.vpc_id
-  name          = "blog_new"
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all","https-443-tcp"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
