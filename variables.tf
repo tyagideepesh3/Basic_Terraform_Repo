72daa82d24d9188e90ce5ae547accd6b4c567fa7
tyagideepesh3 <64723395+tyagideepesh3@users.noreply.github.com>
2023-03-05 17:04:44 +0530

    variables-setup-commit

--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+variable "ami_filter" {
+    description = "This contains Name and Owner values for ami"
+
+    type = object({
+        name = string
+        owner= string
+    })
+
+    default = {
+        name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+        owner = "979382823631"
+    }
+}
+variable "environment"{
+    description = "Development Environment"
+    
+    type = object ({
+        name           = string
+        network_prefix = string
+    })
+
+    default = {
+        name = "dev"
+        network_prefix = "10.0"
+    }
+}
+variable "min_size" {
+    description = "Minimum Size"
+
+    default = 1
+}
+variable "max_size" {
+    description = "Maximum Size"
+
+    default = 2
+}
